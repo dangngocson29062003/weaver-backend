e8744686e28aa8b41a65835a765a65038d94e4fd
Son Dang <dangngocson29062003@gmail.com>
2026-05-08 17:06:24 +0700

    refactor(auth): move current password validation to service layer

--- src/main/java/com/weaver/weaver_backend/dto/request/user/PasswordRequest.java
+++ src/main/java/com/weaver/weaver_backend/dto/request/user/PasswordRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record PasswordRequest(
-        @NotBlank(message = "Current password is required")
         String currentPassword,
         @NotBlank(message = "New password is required")
         @Length(min = 8, message = "New password must be at least 8 characters long")
